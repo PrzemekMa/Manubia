1370dd562ab5275d0a14bba9f4f1b306915a9815
PrzemekMa <111079008+PrzemekMa@users.noreply.github.com>
2022-08-11 19:36:50 +0200

    Add files via upload

---
+++ FarmasiRO/FarmasiRO/ProductCategories.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace FarmasiRO
+{
+  
+    internal class ProductCategories
+    {
+        private const string BaseURL = "https://farmasi.ro";
+
+        public void GetCategories()
+        {
+            var web = new HtmlWeb();
+            var document = web.Load(BaseURL);
+
+            var tablerows = document.QuerySelectorAll("div.productMenuHeader"); //znalezienie przy pomocy selektorow css diva z linkiem do kategorii produktu
+
+            foreach(var tablerow in tablerows)
+            {
+                var href = tablerow.QuerySelector("a").Attributes["href"].Value; //wybranie z selektora "a" wartosc tekstowa parametru href w ktorym znajduje sie odnosnik do kategorii
+                Console.WriteLine(tablerow.InnerText); // wypisanie nazwy kategorii
+                Console.WriteLine($"{BaseURL}{href}"); // wypisanie linku do kategorii
+            }
+
+        }
+    }
+}
---
+++ FarmasiRO/FarmasiRO/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FarmasiRO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var getcat = new ProductCategories(); // utworzenie nowego obiektu klasy "ProductCategories"
+            getcat.GetCategories(); // wywolanie meteody 
+        }
+    }
+}
+/* Nie korzystalem z projektu udostepnionego na gicie poniewaz jego struktura byla dla mnie troszke zbyt skomplikowana, zrozumialem tresc zadania
+ * jednak nie mialem po prostu czasu na odnalezienie sie w tym projekcie, aby wynik w tym projekcie byl taki jaki mial byc w poleceniu zadania
+ * nalezaloby pobrac z linkow wypisanych przez metode GetCategories za pomoca np. selektorow css (jedyna znana mi metoda) linki do produktow z 
+ * podanych kategorii nastepnie w kazdym z tych produktow (selektor document.querySelectorAll("#divProductList > div:nth-child(1)")), problemem 
+ * byloby dla mnie na pewno to ze na stronie nie wyswietlaja sie od razu wszystkie produkty z podanej kategorii.
+ * Nastepnie w tych produktach nalezaloby odnalezc jednostke miary i cene, nie udalo mi sie znalezc odpowiedzialnego za to selektora.
+ * Fragmen ktory wykonalem zajal mi 1 godzine i  40 minut wraz z nauczeniem sie podstaw c#, poniewaz nigdy wczesniej z niego nie korzystalem.
+ * Mam nadzieje ze moj komentarz pomorze w odpowiedniej ocenie. Pozdrawiam */ 
